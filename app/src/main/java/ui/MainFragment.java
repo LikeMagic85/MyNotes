0f79bece06bd6bb003276bf2d3af5581ae3e3e00
LikeMagic85 <like-magic@yandex.ru>
2022-07-11 15:30:23 +0300

    Done

--- app/src/main/java/ui/MainFragment.java
+++ app/src/main/java/ui/MainFragment.java
@@ -8,14 +8,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.example.mynotes.FullNoteFragment;
 import com.example.mynotes.R;
 
 import java.util.ArrayList;
